7892bbecf4e32b1ac313a0362259dfa8eef7020e
Santosh <santoshkushwah01012001@gmail.com>
2023-05-11 11:02:13 +0530

    pull the code

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,10 @@
+// coded by santosh
 import React from 'react'
 import "./Sidebar.css"
 
 function Sidebar() {
   return (
-    <div className="w-[15%] bg-[#202020]">
+    <div className="w-[15%] bg-[#202020] h-screen text-white">
       Sidebar
     </div>
   )
