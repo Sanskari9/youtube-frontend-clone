19916f9582fdb2cf091aa19fdfe1aa701f79dc48
rajmal varma <rajmalvarma9@gmail.com>
2023-05-12 20:52:08 +0530

    all UI part complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,27 @@
 
                             //  Navbar is made by Rajmal Varma ...................
 import './navbar.css'
-import { Search } from '@mui/icons-material'
-<<<<<<< HEAD:src/components/Topbar/Navbar.js
-import {SearchOutlinedIcon} from '@mui/icons-material/SearchOutlinedIcon'
-import {AccountCircleOutlinedIcon} from '@mui/icons-material/AccountCircleOutlined'
-import styled from 'styled-components'
-import { Container } from '@mui/material'
-=======
+import { Input, Search } from '@mui/icons-material'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
->>>>>>> 7a07678cb6be04314411c3a7af334b7ab47a37ba:src/components/Navbar.js
 import React from 'react'
+import { Container } from '@mui/system';
 
 
 const Navbar = () => {
   return (
-    <>
-    <div>
-      <Search>
-    <input type="" placeholder=' Search for videos'/>
-   <SearchOutlinedIcon/>
+    
+    <Container className="container">
+      <div className = "subcontainer">
+      <Search className = "Search" >
+        <input placeholder='search for videos'/>
+        
       </Search>
-      <button>
-        <AccountCircleOutlinedIcon/>
-        Sign In
-      </button>
-    </div>
-    </>
+      </div>
+      
+
+   </Container>
+   
   )
 }
 
